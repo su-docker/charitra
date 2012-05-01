77ebd0da0a2dff334922eaddac1bde27d271552f
Sudhakar <sudhakar82@gmail.com>
2012-05-01 15:26:50 +0530

    Constrained maximum possible zoom

--- script/map.js
+++ script/map.js
@@ -7,17 +7,23 @@ function initMap() {
 }
 
 function initSize() {
+
     $("#map").parent().attr("height", $(document).height());
     $("#map").parent().attr("width", $(document).width());
 }
 
 function initZoom() {
     console.log("initialized zoom");
-    d3.select("#map").call(d3.behavior.zoom().on("zoom", redraw));
-//                    .attr("transform", "scale(1.75,1.75)");
+    var maxHeight = $(document).height(),
+        maxWidth = $(document).width(),
+        zoomBehaviour = d3.behavior.zoom()
+            .scaleExtent([1, 20])
+            .on("zoom", redraw);
+    d3.select("#map").call(zoomBehaviour);
 }
 
 function redraw() {
+    console.log(d3.event.scale);
     translation = [d3.event.translate[0] * 1.5, d3.event.translate[1] * 1.5]
     d3.select("#map").attr("transform", "translate(" + d3.event.translate + ")" + "scale(" + (d3.event.scale) + ")");
 }
